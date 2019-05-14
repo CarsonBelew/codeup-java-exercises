9c2d05d176a800d518641a726ebafacab6490ede
CarsonBelew <carbelew@gmail.com>
2019-05-14 15:38:43 -0500

    table of powers

--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -1,18 +1,16 @@
+import java.util.Scanner;
 public class ControlFlowExercises {
     public static void main(String[] args) {
-        for (int i = 1; i <= 100; i++){
-            if (i % 15 == 0){
-                System.out.println("Fizz Buzz");
-            }
-            else if (i % 3 == 0){
-                System.out.println("Fizz");
-            }
-            else if (i % 5 == 0){
-                System.out.println("Buzz");
-            }
-            else {
-                System.out.println(i);
-            }
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Give me a number to calculate what it is squared and cubed");
+        int userInput = scan.nextInt();
+        for (int i = 0; i <= userInput; i++){
+            if (i == 0){
+                System.out.printf("|%-8s|%-6s|%-8s|\n|--------|------|--------|\n","Number","Squared","Cubed");
+            }else if (i == userInput){
+                System.out.printf("|%-8s|%-6s|%-8s|\n|________|______|________|\n",i,i*i,i*i*i);
+            }else{
+                System.out.printf("|%-8s|%-6s|%-8s|\n",i,i*i,i*i*i);}
         }
     }
 }
