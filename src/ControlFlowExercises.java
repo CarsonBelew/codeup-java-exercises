1c38341cee3dfc204c4c1f8bbe9594f0f0d8642a
CarsonBelew <carbelew@gmail.com>
2019-05-14 14:47:16 -0500

    for loop of the do while

--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -1,9 +1,7 @@
 public class ControlFlowExercises {
     public static void main(String[] args) {
-        long i = 2;
-        do {
-            System.out.println(i + "\n");
-            i*=i;
-        } while (i <= 1000000);
+        for (long i = 2; i <= 1000000; i*=i){
+            System.out.println(i);
+        }
     }
 }
