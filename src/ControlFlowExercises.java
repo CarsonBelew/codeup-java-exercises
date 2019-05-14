e5fa1bde94ff789cb6121d1f5b6bde4f48afb5e5
CarsonBelew <carbelew@gmail.com>
2019-05-14 15:55:16 -0500

    number grades into letter grades

--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -2,15 +2,22 @@ import java.util.Scanner;
 public class ControlFlowExercises {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
-        System.out.println("Give me a number to calculate what it is squared and cubed");
+        System.out.println("Give me a number to between 1 and 100");
         int userInput = scan.nextInt();
-        for (int i = 0; i <= userInput; i++){
-            if (i == 0){
-                System.out.printf("|%-8s|%-6s|%-8s|\n|--------|------|--------|\n","Number","Squared","Cubed");
-            }else if (i == userInput){
-                System.out.printf("|%-8s|%-6s|%-8s|\n|________|______|________|\n",i,i*i,i*i*i);
-            }else{
-                System.out.printf("|%-8s|%-6s|%-8s|\n",i,i*i,i*i*i);}
+        if ((userInput >=88)){
+            System.out.println("Congrats. You have an A in the class!");
+        }
+        else  if (userInput >=80){
+            System.out.println("Congrats. You have a B in the class!");
+        }
+        else if (userInput >= 67){
+            System.out.println("Well, you have a C...");
+        }
+        else if (userInput >= 60){
+            System.out.println("Well, you have a D and it is starting to get ridiculous..");
+        }
+        else {
+            System.out.println("Welp you Failed. Bye!");
         }
     }
 }
