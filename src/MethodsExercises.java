27c4827b3b8c7b8a54a12ff15db16a67223eecfc
CarsonBelew <carbelew@gmail.com>
2019-05-15 15:23:49 -0500

    #1

--- src/MethodsExercises.java
+++ src/MethodsExercises.java
@@ -1,2 +1,27 @@
 public class MethodsExercises {
+    public static void main(String[] args) {
+        addition(5,5);
+        subtraction(15,5);
+        multiplication(5,5);
+        division(10,5);
+        modulus(50,2);
+    }
+//    1
+    public static void addition(int a, int b){
+        System.out.println(a + b);
+    }
+    public static void subtraction(int a, int b){
+        System.out.println(a - b);
+    }
+    public static void multiplication(int a, int b){
+        System.out.println(a * b);
+    }
+    public static void division(int a, int b){
+        System.out.println(a / b);
+    }
+    public static void modulus(int a, int b){
+        System.out.println(a % b);
+    }
+//    2
+
 }
