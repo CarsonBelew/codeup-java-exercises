a96066c88431772f06fe9be4efd33aebcf96a898
CarsonBelew <carbelew@gmail.com>
2019-05-15 11:56:22 -0500

    added the last problem

--- src/StringExercise.java
+++ src/StringExercise.java
@@ -2,9 +2,11 @@ public class StringExercise {
     public static void main(String[] args) {
 //        String message = "We don't need no education";
 //        System.out.println(message.replace("education", "thought control"));
-        String message = "Check \"this\" out!,";
-        System.out.println(message);
+//        String message = "Check \"this\" out!,";
+//        System.out.println(message);
 //        System.out.println(message.substring());
+        String message = "I can do backslashes \\, double backslashes \\\\,\n" +
+                "and the amazing triple backslash \\\\\\!";
 
     }
 }
