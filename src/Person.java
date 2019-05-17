c03be022f156607032290728b950d4f5a333f78c
CarsonBelew <carbelew@gmail.com>
2019-05-17 14:11:12 -0500

    OOP exercises

--- src/Person.java
+++ src/Person.java
@@ -1,9 +1,35 @@
 public class Person {
-        public String firstName;
-        public String lastName;
+    private String name;
 
-        public String sayHello() {
-            return String.format("Hello from %s %s!", this.firstName, this.lastName);
-        }
+    public String getName(){
+        return name;
+    }
+
+    public void setName(String name){
+        this.name = name;
+    }
+
+    public void sayHello(){
+
+        System.out.println("Hello my name is ");
+    }
+
+    public Person(String name){
+        this.name = name;
+    }
+
+    public static void main(String[] args) {
+        Person person1 = new Person("John");
+        Person person2 = person1;
+        Person name1 = new Person("Carson");
+        System.out.println(person1.getName());
+        System.out.println(person2.getName());
+        person2.setName("Jane");
+        System.out.println(person1.getName());
+        System.out.println(person2.getName());
+        System.out.println("Hello my name is "+name1.getName());
+
+    }
+
 
 }
