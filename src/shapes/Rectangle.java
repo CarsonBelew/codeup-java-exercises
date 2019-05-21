bb0b5567e2c93d0aea9054e219588981e5f03e94
CarsonBelew <carbelew@gmail.com>
2019-05-21 15:10:35 -0500

    cleaned up unused code

--- src/shapes/Rectangle.java
+++ src/shapes/Rectangle.java
@@ -9,21 +9,6 @@ public class Rectangle {
         this.width = width;
     }
 
-    public double getLength() {
-        return length;
-    }
-
-    public void setLength(double length) {
-        this.length = length;
-    }
-
-    public double getWidth() {
-        return width;
-    }
-
-    public void setWidth(double width) {
-        this.width = width;
-    }
 
     public double getArea(){
         return length * width;
